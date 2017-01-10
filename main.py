6dc62d50933911068d9bbf79316a3f5d0dfc6bdb
ravenusmc <mcuddy77@gmail.com>
2017-01-10 10:10:54 -0500

    added data class

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import pandas as pd
 import numpy as np
 
-
+#importing all files that will be used in this program.
+from valid import *
+from data import *
 
 
 ### Functions that will support the main program below here.
@@ -11,10 +13,12 @@ def quit():
     print("\033c")
     print('Sorry you don"t want to use the program')
     print('Maybe next time...Good Bye!')
+    print()
 
 
 ### Main Program functions here.
 
+#This is the main function of the program that will be used to run it.
 def main():
     print("\033c")
     print('--------------------------------------------------------------')
@@ -24,12 +28,22 @@ def main():
     print()
     print('1. Use Program')
     print('2. Quit')
-    choice = input('What is your choice: ')
+    choice = int(input('What is your choice: '))
+    print(choice)
+    while not main_valid(choice):
+        print("That choice is not acceptable!")
+        choice = int(input('What is your choice: '))
     if choice == 1:
         start_program()
     elif choice == 2:
         quit()
 
-games = pd.read_csv('games.csv')
+def start_program():
+    print("\033c")
+    data = Data()
+    print(data.games.head())
+
+
+#games = pd.read_csv('games.csv')
 
 main()
---
+++ valid.py
@@ -0,0 +1,7 @@
+#This file will contain all of the functions for validation in my program
+
+def main_valid(choice):
+    if choice == 1 or choice == 2:
+        return True
+    else:
+        return False
